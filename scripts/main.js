bbe18fb976f7eb652be350024c527b3e41ba0a9d
lproductions <l.productionsuk@gmail.com>
2015-07-07 11:46:13 +0000

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+var UserText;
+var myButton;
+
+var image;
+
+$(document).ready(function(){
+  $("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  7000);
+	UserText = document.querySelector('#usermessage');
+		 myButton = document.querySelector('button');
+		
+
+
+
+
+if(localStorage.getItem('name')) {
+  var storedName = localStorage.getItem('name');
+  UserText.innerHTML = "Welcome,&nbsp;" + storedName;
+}
+
+var setUserName = function(){
+var myName = prompt('Please enter your name.');
+localStorage.setItem('name', myName)
+UserText.innerHTML = "Welcome, " + myName;
+}
+
+myButton.onclick = function() {
+ setUserName()
+}
+});
+
+
+
+
+
